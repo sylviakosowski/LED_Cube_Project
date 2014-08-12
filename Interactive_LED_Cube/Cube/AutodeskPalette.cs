5e63d7ad0c2e67ab54d0fac06cbd6f012d628fd9
Sylvia Kosowski <sylviakosowski@gmail.com>
2014-08-12 12:38:47 -0700

    Autodesk palette created

---
+++ Interactive_LED_Cube/Cube/AutodeskPalette.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_LED_Cube
+{
+    public class AutodeskPalette : ColorPalette
+    {
+        public RGBColor MapCoordToColor(Coordinate c)
+        {
+            //int R = 256 - 32 * (c.X + 1);
+            //int G = 256 - 32 * (c.Y + 1);
+            //int B = 256 - 32 * (c.Z + 1);
+
+            int R = 6 + 16 * (c.X + 1);
+            int G = 150 + 4 * (c.Y + 1);
+            int B = 215 - 19 * (c.Z + 1);
+
+            return new RGBColor((byte)R, (byte)G, (byte)B);
+        }
+    }
+}
